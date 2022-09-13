9ff05cddf027978148a2067dbec872824ddcf93e
Florieta <florieta.pencheva@gmail.com>
2022-09-13 12:46:18 +0300

    Role Post method added

---
+++ Warehouse.Infrastructure/Data/Repositories/ApplicatioDbRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Infrastructure.Data.Common;
+
+namespace Warehouse.Infrastructure.Data.Repositories
+{
+    
+        public class ApplicatioDbRepository : Repository, IApplicatioDbRepository
+        {
+            public ApplicatioDbRepository(ApplicationDbContext context)
+            {
+                this.Context = context;
+            }
+        }
+    
+}
---
+++ Warehouse/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Core.Constants;
+
+
+namespace Warehouse.Areas.Admin.Controllers
+{
+    [Authorize(Roles = UserConstants.Roles.Administrator)]
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+
+    }
+}
